432aa3e7b781a3b0b8568248fd813a0a26456693
Cristian Silva <chrissilva9420@gmail.com>
2020-11-16 16:41:12 -0600

    Restructured the base of the user and token models.

---
+++ lib/models/json/Token.dart
@@ -0,0 +1,18 @@
+class Token {
+  String refresh;
+  String access;
+
+  Token({this.refresh, this.access});
+
+  Token.fromJson(Map<String, dynamic> json) {
+    refresh = json['refresh'];
+    access = json['access'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['refresh'] = this.refresh;
+    data['access'] = this.access;
+    return data;
+  }
+}
---
+++ lib/models/json/User.dart
@@ -0,0 +1,48 @@
+class User {
+  int id;
+  bool isSuperuser;
+  String username;
+  String firstName;
+  String lastName;
+  String email;
+  bool isStaff;
+  bool isActive;
+  String dateJoined;
+
+  User(
+      {this.id,
+        this.isSuperuser,
+        this.username,
+        this.firstName,
+        this.lastName,
+        this.email,
+        this.isStaff,
+        this.isActive,
+        this.dateJoined});
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    isSuperuser = json['is_superuser'];
+    username = json['username'];
+    firstName = json['first_name'];
+    lastName = json['last_name'];
+    email = json['email'];
+    isStaff = json['is_staff'];
+    isActive = json['is_active'];
+    dateJoined = json['date_joined'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['is_superuser'] = this.isSuperuser;
+    data['username'] = this.username;
+    data['first_name'] = this.firstName;
+    data['last_name'] = this.lastName;
+    data['email'] = this.email;
+    data['is_staff'] = this.isStaff;
+    data['is_active'] = this.isActive;
+    data['date_joined'] = this.dateJoined;
+    return data;
+  }
+}
