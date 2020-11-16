41735a80b661d3c2e8b83826831ad01f3d988711
Cristian Silva <chrissilva9420@gmail.com>
2020-11-16 16:24:43 -0600

    Added Welcome Page to give user option to sign up or login.

---
+++ lib/pages/welcomePage.dart
@@ -0,0 +1,193 @@
+import 'package:bigshop/auth.dart';
+import 'package:bigshop/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:bigshop/pages/loginPage.dart';
+import 'package:bigshop/pages/signup.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+class WelcomePage extends StatefulWidget {
+  WelcomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Theme.of(context).accentColor.withAlpha(100),
+                  offset: Offset(2, 4),
+                  blurRadius: 8,
+                  spreadRadius: 2)
+            ],
+            color: Colors.white),
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 20, color: Theme.of(context).primaryColor),
+        ),
+      ),
+    );
+  }
+
+  Widget _signUpButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SignUpPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          border: Border.all(color: Colors.white, width: 2),
+        ),
+        child: Text(
+          'Register now',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _label() {
+    return Container(
+        margin: EdgeInsets.only(top: 40, bottom: 20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Quick login with Touch ID',
+              style: TextStyle(color: Colors.white, fontSize: 17),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Icon(Icons.fingerprint, size: 90, color: Colors.white),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Touch ID',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: 'B',
+          style: GoogleFonts.portLligatSans(
+            textStyle: Theme.of(context).textTheme.display1,
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+          children: [
+            TextSpan(
+              text: 'ig',
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+            TextSpan(
+              text: 'Shop',
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ]),
+    );
+  }
+
+  Widget _logo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Container(
+          color: Colors.black12,
+          margin: new EdgeInsets.all(30.0),
+          width: 250.0,
+          height: 250.0,
+          child: new Image.asset(
+            'images/BigshopLogo.png',
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthState>(
+        builder: (context, value, child) {
+      return value.isLoggedIn ? HomePage() : Scaffold(
+      body:SingleChildScrollView(
+        child:Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.grey.shade200,
+                      offset: Offset(2, 4),
+                      blurRadius: 5,
+                      spreadRadius: 2)
+                ],
+                gradient: LinearGradient(
+                    // begin: Alignment.topCenter,
+                    // end: Alignment.bottomCenter,
+                    //colors: [Color(0xfffbb448), Color(0xffe46b10)]
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Theme.of(context).accentColor,
+                    Theme.of(context).secondaryHeaderColor,
+                    Theme.of(context).primaryColor
+                  ],
+       )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //_title(),
+                _logo(),
+                // SizedBox(
+                //   height: 80,
+                // ),
+                _submitButton(),
+                SizedBox(
+                  height: 20,
+                ),
+                _signUpButton(),
+                SizedBox(
+                  height: 20,
+                ),
+                //_label()
+              ],
+            ),
+          ),
+      ),
+    );
+  });
+}
